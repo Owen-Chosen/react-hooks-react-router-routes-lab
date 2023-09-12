1c18c37e8960d8b5432441f737518141589e0491
owen <owen.chosen@gmail.com>
2023-09-12 03:18:46 +0100

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,17 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route path="/" exact>{<Home />}</Route>
+        <Route path="/actors" exact>{<Actors />}</Route>
+        <Route path="/directors" exact>{<Directors />}</Route>
+        <Route path="/movies" exact>{<Movies />}</Route>
+      </Switch>
+    </div>
+  )
 }
 
 export default App;
